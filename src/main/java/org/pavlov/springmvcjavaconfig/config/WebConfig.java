acb1cdd40622dcd20400b2a613de787561a39c01
pavlov <pavlov@pavlov.matrix.local>
2015-03-12 13:49:57 +0400

    Spring MVC java config template 1.0

--- src/main/java/org/pavlov/springmvcjavaconfig/config/WebConfig.java
+++ src/main/java/org/pavlov/springmvcjavaconfig/config/WebConfig.java
@@ -5,13 +5,18 @@
  */
 package org.pavlov.springmvcjavaconfig.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 /**
  *
@@ -30,13 +35,27 @@ class WebConfig extends WebMvcConfigurerAdapter{
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         // super.configureViewResolvers(registry); //To change body of generated methods, choose Tools | Templates.
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".html");
-        resolver.setExposeContextBeansAsAttributes(true);
-        registry.viewResolver(resolver);
-
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        resolver.setPrefix("/WEB-INF/views/");
+//        resolver.setSuffix(".xhtml");
+//        resolver.setExposeContextBeansAsAttributes(true);
+//        registry.viewResolver(resolver);
+        
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".xhtml");
+        templateResolver.setTemplateMode("HTML5");
+   
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        
+        registry.viewResolver(viewResolver);
+    
+    
     }
-
+ 
     
 }
